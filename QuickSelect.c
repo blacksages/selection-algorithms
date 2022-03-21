fe83c80a2e7e8122112dc37e35b272c023a72be3
blacksages <blacksages@live.fr>
2022-03-21 14:37:12 +0100

    return quickeselect

--- QuickSelect.c
+++ QuickSelect.c
@@ -6,7 +6,8 @@
 #include <stddef.h>
 #include "Select.h"
 
-static void swap(int *array, int index_1, int index_2){
+static void swap(int *array, int index_1, int index_2)
+{
   int tmp = array[index_1];
   array[index_1] = array[index_2];
   array[index_2] = tmp;
@@ -15,12 +16,15 @@ static void swap(int *array, int index_1, int index_2){
 /*
 
 */
-static int partition(int *array, int p, int r){
+static int partition(int *array, int p, int r)
+{
   int x = array[r];
   int i = p - 1; //-
   int j = p;
-  for( ; j < r; j++){
-    if(array[j] <= x){
+  for (; j < r; j++)
+  {
+    if (array[j] <= x)
+    {
       i++;
       swap(array, i, j);
     }
@@ -32,24 +36,30 @@ static int partition(int *array, int p, int r){
 /*
 
 */
-static int quickselect(int *array, int p, int r, int k){
-  if(p < r){
+static int quickselect(int *array, int p, int r, int k)
+{
+  if (p < r)
+  {
     int q = partition(array, p, r);
-    if(k == q){
+    if (k == q)
+    {
       return array[q];
     }
-    if(k < q){
-      //Left
-      quickselect(array, p, q - 1, k);
+    if (k < q)
+    {
+      // Left
+      return quickselect(array, p, q - 1, k);
     }
-    if(k > q){
-      //Right
-      quickselect(array, q + 1, r, k);
+    if (k > q)
+    {
+      // Right
+      return quickselect(array, q + 1, r, k);
     }
   }
+  return -1;
 }
 
-int select(int* array, size_t length, size_t k)
+int select(int *array, size_t length, size_t k)
 {
-    return quickselect(array, 0, length - 1, k);
+  return quickselect(array, 0, length - 1, k);
 }
